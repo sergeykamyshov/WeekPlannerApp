616d8feaa12443eefcf5c95f33949eacc7f133a1
sergeykamyshov <kamishovsergey@gmail.com>
2018-03-04 23:18:00 +0300

    Fixed an error when after double deleting tasks the app restored empty task

--- app/src/main/java/ru/sergeykamyshov/weekplanner/activities/CardActivity.java
+++ app/src/main/java/ru/sergeykamyshov/weekplanner/activities/CardActivity.java
@@ -131,7 +131,7 @@ public class CardActivity extends AppCompatActivity implements TaskItemTouchHelp
             snackbar.addCallback(new Snackbar.Callback() {
                 @Override
                 public void onDismissed(Snackbar transientBottomBar, int event) {
-                    if (event != DISMISS_EVENT_ACTION) {
+                    if (event != DISMISS_EVENT_ACTION && event != DISMISS_EVENT_CONSECUTIVE) {
                         mTaskSharedPreferencesUtils.clearData();
                     }
                 }
